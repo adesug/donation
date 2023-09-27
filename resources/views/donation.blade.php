7d071090bbb264fb4f2ae403e26ca88d4b96225f
adesug <adesugiantoro89@gmail.com>
2023-09-27 18:20:41 +0700

    update url sandbox snappay

--- resources/views/donation.blade.php
+++ resources/views/donation.blade.php
@@ -99,7 +99,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js">
     </script>
     <script type="text/javascript" 
-     src="https://app.midtrans.com/snap/snap.js" 
+     src="https://app.sandbox.midtrans.com/snap/snap.js" 
      data-client-key="{{ config('services.midtrans.clientKey')}}"> 
     </script>
     <script>
